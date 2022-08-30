5f37a88fe5d2bc28a4be2da915ea98317b7a8bbe
Toby Liu <toby.liu@mac-f7t9rp.home>
2022-08-29 17:45:14 -0700

    fix api

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    fetch('/api/item/1').then((res) => {
+      res.text().then((text) => setData(text));
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{data}</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
