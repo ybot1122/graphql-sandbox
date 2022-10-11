f77f7daf39a6406a71868b6cbcea17f4fea9248f
Toby Liu <ybot1122@gmail.com>
2022-10-11 16:52:21 -0700

    Add support for hulu images

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -8,8 +8,10 @@ export const Card = ({ title, description, brand, image }) => {
   const icon_src = brand === 'Apple Tv' ? appletv_icon : hulu_icon;
   return (
     <div className="card">
-      <img src={icon_src} alt={brand} />
-      <img src={image} alt={title} />
+      <div className="thumbnail-container">
+        <img src={image} className="thumbnail-img" alt={title} />
+        <img src={icon_src} className="brand-img" alt={brand} />
+      </div>
       <h3>{title}</h3>
       <p>{description}</p>
     </div>
